7d7ed270186f5039ff62ce74aff8915ef289cadf
DinaSami <dina.faur@yahoo.com>
2021-07-26 20:41:32 +0300

    done

--- Binary Tree and BST Implementation/__test__/tree.test.js
+++ Binary Tree and BST Implementation/__test__/tree.test.js
@@ -84,4 +84,18 @@ describe('testing Tree Class ', () => {
     let getMax = tree.getMax();
     expect(getMax).toBeFalsy();
   });
+  it('tree-breadth-first method', () => {
+    const tree = new Tree();
+    tree.root = new Node(2);
+    tree.root.left = new Node(7);
+    tree.root.left.left = new Node(2);
+    tree.root.left.right = new Node(6);
+    tree.root.left.right.left = new Node(5);
+    tree.root.left.right.right = new Node(11);
+    tree.root.right = new Node(5);
+    tree.root.right.right = new Node(9);
+    tree.root.right.right.left = new Node(4);
+    let breadthFirst = tree.breadthFirst();
+    expect(breadthFirst).toEqual([2,7,5,2,6,9,5,11,4]);
+  });
 });
